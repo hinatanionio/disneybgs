6f9976713126f15669a67dcc85183084f1de816c
Hinata Nio <hinatanio@HinatanoMacBook-Air.local>
2022-09-23 01:24:45 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function($) {
+    $('.more-button').on('click', function(){
+      if ($('.open-box').is(':hidden')) {
+        $('.open-box').slideDown();
+        $(this).text('CLOSE');
+        $(this).css('background-color', '#808080');
+      }else{
+        $('.open-box').slideUp(); 
+        $(this).text('MENU');
+        $(this).css('background-color', '#2E4056');
+      }
+    });
+});
