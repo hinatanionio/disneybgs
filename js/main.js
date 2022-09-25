9090d096790b8d6b005a14d551dcafc0e1014bfb
Hinata Nio <hinatanio@HinatanoMacBook-Air.local>
2022-09-25 16:09:07 +0900

    third commit 9/25

--- js/main.js
+++ js/main.js
@@ -10,4 +10,16 @@ $(function($) {
         $(this).css('background-color', '#2E4056');
       }
     });
+
+    $(".modal-button").modaal();
+
+    $(window).scroll(function(){
+      var targetElement = $('.fadein').offset().top;
+      var scroll = $(window).scrollTop(); 
+      var windowHeight = $(window).height();
+      if (scroll > targetElement - windowHeight + 200) {
+          $('.fadein').css('opacity','1');
+          $('.fadein').css('trandform', 'translateY(0)');
+      }
+    });
 });
