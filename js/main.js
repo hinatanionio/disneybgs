1916ff6b9663a803b7c971b5c1db1e9d1980341c
Hinata Nio <hinatanio@HinatanoMacBook-Air.local>
2022-09-27 18:06:44 +0900

    コミットした内容をメモ

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
+
+
 $(function($) {
-    $('.more-button').on('click', function(){
-      if ($('.open-box').is(':hidden')) {
-        $('.open-box').slideDown();
-        $(this).text('CLOSE');
-        $(this).css('background-color', '#808080');
-      }else{
-        $('.open-box').slideUp(); 
-        $(this).text('MENU');
-        $(this).css('background-color', '#2E4056');
-      }
-    });
+  $('.more-button').on('click', function(){
+    if ($('.open-box').is(':hidden')) {
+      $('.open-box').slideDown();
+      $(this).text('CLOSE');
+      $(this).css('background-color', '#808080');
+    }else{
+      $('.open-box').slideUp(); 
+      $(this).text('MENU');
+      $(this).css('background-color', '#2E4056');
+    }
+  });
 
-    $(".modal-button").modaal();
+  $(".modal-button").modaal();
 
-    $(window).scroll(function(){
-      var targetElement = $('.fadein').offset().top;
-      var scroll = $(window).scrollTop(); 
-      var windowHeight = $(window).height();
-      if (scroll > targetElement - windowHeight + 200) {
-          $('.fadein').css('opacity','1');
-          $('.fadein').css('trandform', 'translateY(0)');
-      }
-    });
 });
